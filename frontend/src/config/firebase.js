f91e19ae6c2ca7958e924c454a15a61c7370931e
Ritik Thakur <ritik.thakur.che22@itbhu.ac.in>
2025-10-02 22:32:01 +0530

    Removed the config files

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,23 +1,17 @@
-import { getAuth } from "firebase/auth";
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnBxoUrF2zz3C6AGYgbI0zLyEFYUFvd94",
-  authDomain: "chatterbox-43d4a.firebaseapp.com",
-  projectId: "chatterbox-43d4a",
-  storageBucket: "chatterbox-43d4a.appspot.app",
-  messagingSenderId: "815454325255",
-  appId: "1:815454325255:web:9d2ff0a0efb13bd30f78a2",
-  measurementId: "G-4BDZQB4WC7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
